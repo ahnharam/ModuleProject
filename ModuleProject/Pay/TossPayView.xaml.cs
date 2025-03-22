e299bf386177519c3bfb8e5a199d11e8c5392b0c
BOOK-2E3AI3OPRK\blues <ahr1025@naver.com>
2025-03-22 14:56:10 +0900

    토스페이를 위한 뷰

--- ModuleProject/Pay/TossPayView.xaml.cs
+++ ModuleProject/Pay/TossPayView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows;
+using System;
+using System.Windows.Controls;
 
 namespace ModuleProject.Pay
 {
@@ -10,6 +13,26 @@ namespace ModuleProject.Pay
         public TossPayView()
         {
             InitializeComponent();
+        }
+
+        private async void OnPayButtonClick(object sender, RoutedEventArgs e)
+        {
+            await WebViewControl.EnsureCoreWebView2Async(null);
+            WebViewControl.Visibility = Visibility.Visible;
+            //http://localhost:5173/
+            WebViewControl.Source = new Uri("http://localhost:5173/");
+        }
+
+        private void WebViewControl_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (WebViewControl.Source.ToString().Contains("success"))
+            {
+                MessageBox.Show("결제 성공");
+            }
+            else if (WebViewControl.Source.ToString().Contains("fail"))
+            {
+                MessageBox.Show("결제 실패");
+            }
         }
     }
 }
